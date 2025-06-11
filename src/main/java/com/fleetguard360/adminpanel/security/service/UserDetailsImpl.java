d37747307d3c3d666d085b13f9066da3ddf9ac8b
Brahian2215 <109265618+Brahian2215@users.noreply.github.com>
2025-06-11 18:43:26 -0500

    Update UserDetailsImpl.java

--- src/main/java/com/fleetguard360/adminpanel/security/service/UserDetailsImpl.java
+++ src/main/java/com/fleetguard360/adminpanel/security/service/UserDetailsImpl.java
@@ -109,7 +109,7 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean hashCode(Object o) {
         if (this == o)
             return true;
         if (o == null || getClass() != o.getClass())
